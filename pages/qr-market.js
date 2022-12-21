39b5017bcb8380c2d00e554bbc1a846b700c1458
Mertcan Kose <mertcankose97@outlook.com>
2022-12-21 23:02:38 +0300

    first commit

---
+++ pages/qr-market.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+const QrMarket = () => {
+  useEffect(() => {
+    if (navigator.userAgent.match(/Android/i)) {
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=com.aybumobile";
+    } else {
+      window.location.href =
+        "https://apps.apple.com/us/app/ayb%C3%BC-mobile/id1658659307";
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Redirecting...</h1>
+    </div>
+  );
+};
+
+export default QrMarket;
